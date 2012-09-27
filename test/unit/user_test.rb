b9dff626b951737f8ffcf015cef792a607e1220a
Joel Klabo <joelklabo@gmail.com>
2012-09-27 12:36:48 -0700

    adding user fixtures and tests for presence of all data

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "User needs an email" do
+    user = User.new :name => 'joelsbeardalso'
+    assert !user.save, "You saved the post without an email"
+  end
+  test "User needs a name" do
+    user = User.new :email => 'joel@dumb.com'
+    assert !user.save, "You saved the post without a name"
+  end
+  test "User should save with a name and an email" do
+    user = User.new :name => 'joel', :email => 'joel@dumb.com'
+    assert user.save, "You couldn't save the user with an email and a name"
+  end
 end
